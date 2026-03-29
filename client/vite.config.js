6e6030a42eab424a97c143d0d08d1e54dc4cb0ea
Muneeb Rana <muneebrana497@gmail.com>
2026-03-29 19:54:54 +0500

    update code

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   base: "/",
@@ -11,4 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    'process.env': {}
+  }
 });
