9e9107e73240b0ce50fca19912bae238be789552
Muneeb Rana <muneebrana497@gmail.com>
2026-03-30 16:48:08 +0500

    update code

--- client/src/store/shop/address-slice/index.js
+++ client/src/store/shop/address-slice/index.js
@@ -10,7 +10,7 @@ export const addNewAddress = createAsyncThunk(
   "/addresses/addNewAddress",
   async (formData) => {
     const response = await axios.post(
-      `${import.meta.env.VITE_API_URL}/api/shop/address/add`,
+      `${import.meta.env.VITE_API_URL}/shop/address/add`,
       formData
     );
 
@@ -22,7 +22,7 @@ export const fetchAllAddresses = createAsyncThunk(
   "/addresses/fetchAllAddresses",
   async (userId) => {
     const response = await axios.get(
-      `${import.meta.env.VITE_API_URL}/api/shop/address/get/${userId}`
+      `${import.meta.env.VITE_API_URL}/shop/address/get/${userId}`
     );
 
     return response.data;
