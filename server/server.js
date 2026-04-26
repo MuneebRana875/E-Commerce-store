38aad55bba6656be4e52d783f0b53051a90e3545
Muneeb Rana <muneebrana497@gmail.com>
2026-04-26 22:37:35 +0500

    update code

--- server/server.js
+++ server/server.js
@@ -72,8 +72,10 @@ app.use("/api/shop/review", shopReviewRouter);
 app.use("/api/common/feature", commonFeatureRouter);
 
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`✅ Server is running on port ${PORT}`);
-});
+// app.listen(PORT, "0.0.0.0", () => {
+//     console.log(`✅ Server is running on port ${PORT}`);
+// });
+
+module.exports = app;
